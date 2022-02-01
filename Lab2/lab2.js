1a2681325dfba16ddc3f7052e46f0068a8867e18
Payton Gilbertson <paytong@myaccess.ca>
2022-01-31 21:32:11 -0600

    Glasses look like glasses!!!
    Might be done. Will ask Alex for info in lab

--- Lab2/lab2.js
+++ Lab2/lab2.js
@@ -1,77 +1,190 @@
 // This variable will store the WebGL rendering context
 var gl;
-
-//Uniform Color Index
+var myFirstVao;
 var uColor;
 
-<<<<<<< HEAD
-//Vertex Array Objects (VAOs)
-var flatArrays, smoothArrays;
-
-// Shader Programs
-var smoothProgram, flatProgram;
-
-var bgStripes = [
-    vec2(-0.95, 1.0),
-    vec2(-0.95, -1.0),
-    vec2(-0.85, 1.0),
-    vec2(-0.85, -1.0),
-
-    vec2(-0.75, 1.0),
-    vec2(-0.75, -1.0),
-    vec2(-0.65, 1.0),
-    vec2(-0.65, -1.0),
-
-    vec2(-0.55, 1.0),
-    vec2(-0.55, -1.0),
-    vec2(-0.45, 1.0),
-    vec2(-0.45, -1.0),
-
-    vec2(-0.35, 1.0),
-    vec2(-0.35, -1.0),
-    vec2(-0.25, 1.0),
-    vec2(-0.25, -1.0),
-
-    vec2(-0.15, 1.0),
-    vec2(-0.15, -1.0),
-    vec2(-0.05, 1.0),
-    vec2(-0.05, -1.0),
-
-    vec2(0.05, 1.0),
-    vec2(0.05, -1.0),
-    vec2(0.15, 1.0),
-    vec2(0.15, -1.0),
-
-    vec2(0.25, 1.0),
-    vec2(0.25, -1.0),
-    vec2(0.35, 1.0),
-    vec2(0.35, -1.0),
-
-    vec2(0.45, 1.0),
-    vec2(0.45, -1.0),
-    vec2(0.55, 1.0),
-    vec2(0.55, -1.0),
-
-    vec2(0.65, 1.0),
-    vec2(0.65, -1.0),
-    vec2(0.75, 1.0),
-    vec2(0.75, -1.0),
-
-    vec2(0.85, 1.0),
-    vec2(0.85, -1.0),
-    vec2(0.95, 1.0),
-    vec2(0.95, -1.0)
-];
-
-var leftLens = circleLeft(25);
-var rightLens = circleRight(50);
+var leftLens = circle(50, 0.3, -0.45, 0.3, 0.0, 0.4, -0.45, 0.4, 0.0);
+var rightLens = circle(50, 0.3, 0.45, 0.3, 0.0, 0.4, 0.45, 0.4, 0.0);
+var leftFill = circle(50, 0.35, -0.45, 0.35, 0.0, 0.4, -0.45, 0.4, 0.0);
+var rightFill = circle(50, 0.35, 0.45, 0.35, 0.0, 0.4, 0.45, 0.4, 0.0);
 
 
 var bgTeal = vec4(0.4941176471, 0.6117647059, 0.6, 1.0);
+var bgWhite = vec4(0.7647058824, 0.7137254902, 0.6862745098, 1);
+
+var glassesFrame = vec4(0.98, 0.5, 0.55, 0.9);
+
+var points =
+    [
+
+    ];
+
+var colors =
+    [
+
+    ];
 
 
-=======
->>>>>>> parent of 98e2598 (Added background and basic glasses)
+
+var bgStripeCoords =
+    [
+        vec2(-0.95, 1.0),
+        vec2(-0.95, -1.0),
+        vec2(-0.85, 1.0),
+        vec2(-0.85, -1.0),
+
+        vec2(-0.75, 1.0),
+        vec2(-0.75, -1.0),
+        vec2(-0.65, 1.0),
+        vec2(-0.65, -1.0),
+
+        vec2(-0.55, 1.0),
+        vec2(-0.55, -1.0),
+        vec2(-0.45, 1.0),
+        vec2(-0.45, -1.0),
+
+        vec2(-0.35, 1.0),
+        vec2(-0.35, -1.0),
+        vec2(-0.25, 1.0),
+        vec2(-0.25, -1.0),
+
+        vec2(-0.15, 1.0),
+        vec2(-0.15, -1.0),
+        vec2(-0.05, 1.0),
+        vec2(-0.05, -1.0),
+
+        vec2(0.05, 1.0),
+        vec2(0.05, -1.0),
+        vec2(0.15, 1.0),
+        vec2(0.15, -1.0),
+
+        vec2(0.25, 1.0),
+        vec2(0.25, -1.0),
+        vec2(0.35, 1.0),
+        vec2(0.35, -1.0),
+
+        vec2(0.45, 1.0),
+        vec2(0.45, -1.0),
+        vec2(0.55, 1.0),
+        vec2(0.55, -1.0),
+
+        vec2(0.65, 1.0),
+        vec2(0.65, -1.0),
+        vec2(0.75, 1.0),
+        vec2(0.75, -1.0),
+
+        vec2(0.85, 1.0),
+        vec2(0.85, -1.0),
+        vec2(0.95, 1.0),
+        vec2(0.95, -1.0)
+    ];
+
+var noseBar = [
+    vec2(-0.065, 0.05),
+    vec2(-0.065, -0.01),
+    vec2(-0.04, 0.065),
+    vec2(-0.03, 0.0),
+    vec2(0.0, 0.075),
+    vec2(0.01, 0.0),
+    vec2(0.04, 0.065),
+    vec2(0.02, -0.0),
+    vec2(0.065, 0.05),
+    vec2(0.065, -0.01)
+];
+
+var wings = [
+    vec2(0.85, 0.0),
+    vec2(0.93, 0.025),
+
+    vec2(0.8, 0.2),
+    vec2(0.87, 0.23),
+
+    vec2(0.65, 0.35),
+    vec2(0.72, 0.375),
+    vec2(0.76, 0.4),
+
+    vec2(0.8, -0.22),
+    vec2(0.87, -0.2),
+
+    vec2(0.65, -0.35),
+    vec2(0.745, -0.33), 
+
+
+    vec2(-0.85, 0.0),
+    vec2(-0.93, 0.025),
+
+    vec2(-0.8, 0.2),
+    vec2(-0.87, 0.23),
+
+    vec2(-0.65, 0.35),
+    vec2(-0.72, 0.375),
+    vec2(-0.76, 0.4),
+
+    vec2(-0.8, -0.22),
+    vec2(-0.87, -0.2),
+
+    vec2(-0.65, -0.35),
+    vec2(-0.745, -0.33), 
+
+];
+
+var lensStripes = [
+    vec2(-0.7, 0.0),
+    vec2(-0.3, 0.0),
+
+    vec2(-0.65, 0.12),
+    vec2(-0.29, 0.01),
+    
+    vec2(-0.52, 0.2),
+    vec2(-0.28, 0.02),
+
+    vec2(-0.4, 0.24),
+    vec2(-0.27, 0.03),
+
+    vec2(-0.28, 0.21),
+    vec2(-0.26, 0.04),
+
+    vec2(-0.65, -0.12),
+    vec2(-0.29, -0.01),
+
+    vec2(-0.52, -0.2),
+    vec2(-0.28, -0.02),
+
+    vec2(-0.4, -0.24),
+    vec2(-0.27, -0.03),
+
+    vec2(-0.28, -0.21),
+    vec2(-0.26, -0.04),
+
+
+    vec2(0.2, 0.0),
+    vec2(0.6, 0.0),
+
+    vec2(0.25, 0.12),
+    vec2(0.61, 0.01),
+    
+    vec2(0.38, 0.24),
+    vec2(0.62, 0.02),
+
+    vec2(0.5, 0.26),
+    vec2(0.63, 0.03),
+
+    vec2(0.62, 0.21),
+    vec2(0.64, 0.04),
+
+    vec2(0.25, -0.12),
+    vec2(0.61, -0.01),
+
+    vec2(0.38, -0.24),
+    vec2(0.62, -0.02),
+
+    vec2(0.5, -0.26),
+    vec2(0.63, -0.03),
+
+    vec2(0.62, -0.21),
+    vec2(0.64, -0.04)
+];
+
 window.addEventListener("load", init);
 function init() {
     //find canvas by id name
@@ -85,136 +198,93 @@ function init() {
         alert("Cannot get WebGL2 Rendering Context");
     }
 
-<<<<<<< HEAD
-    //configure WebGL
-    gl.clearColor(0.4941176471, 0.6117647059, 0.6, 1.0);
-
-    // Load and inistialize shaders
-    var smoothProgram = initShaders(gl, "vertex-shader", "fragment-shader");
-    var flatProgram = initShaders(gl, "flat-vertex-shader", "fragment-shader");
-    //gl.useProgram(program);
-=======
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    //Triangle
-    var points =
-        [
-            vec2(0.0, 0.0),
-            vec2(0.5, 0.0),
-            vec2(0.5, 0.5),
-            vec2(-0.5, 0.5),
-            vec2(-1.0, 0.0),
-            vec2(-0.5, -0.5),
-
-            vec2(0.0, 0.0),
-            vec2(0.5, 0.0),
-            vec2(0.5, 0.5),
-            vec2(-0.5, 0.5),
-            vec2(-1.0, 0.0),
-            vec2(-0.5, -0.5)
-
-        ];
 
 
-        var colors =
-        [
-            vec4(1.0, 0.0, 0.0, 1.0), //Red
-            vec4(0.0, 1.0, 0.0, 1.0), //Green
-            vec4(0.0, 0.0, 1.0, 1.0), //Blue
-            vec4(1.0, 1.0, 0.0, 1.0), //Yellow
-            vec4(0.0, 1.0, 1.0, 1.0), //Cyan
-            vec4(1.0, 0.0, 1.0, 1.0), //Magenta
+    bgStripeCoords.start = points.length;
+    points = points.concat(bgStripeCoords);
+    leftLens.start = points.length;
+    points = points.concat(leftLens);
+    rightLens.start = points.length;
+    points = points.concat(rightLens);
+    noseBar.start = points.length;
+    points = points.concat(noseBar);
+    wings.start = points.length;
+    points = points.concat(wings);
+    leftFill.start = points.length;
+    points = points.concat(leftFill);
+    rightFill.start = points.length;
+    points = points.concat(rightFill);
+    lensStripes.start = points.length;
+    points = points.concat(lensStripes);
 
-            vec4(1,1,1,1),
-            vec4(1,1,1,1),
-            vec4(1,1,1,1),
-            vec4(1,1,1,1),
-            vec4(1,1,1,1),
-            vec4(1,1,1,1)
-        ];
+    for (var i = 0; i < bgStripeCoords.length; i++) {
+        colors = colors.concat(bgWhite);
+    }
+
+    for (var i = 0; i < leftLens.length; i++) {
+        colors = colors.concat(glassesFrame);
+    }
+    for (var i = 0; i < rightLens.length; i++) {
+        colors = colors.concat(glassesFrame);
+    }
+    for(var i = 0; i < noseBar.length; i++){
+        colors = colors.concat(glassesFrame);
+    }
+    for(var i = 0; i < wings.length; i++){
+        colors = colors.concat(glassesFrame);
+    }
+    colors = colors.concat(vec4(0, 0, 1, 1));
+    colors = colors.concat(vec4(0, 0, 1, 1));
+
+    for(var i = 0; i < leftFill.length-2; i++){
+        colors = colors.concat(vec4(1,1,1,1));
+    }
+    colors = colors.concat(vec4(0.7, 0, 1, 1));
+    colors = colors.concat(vec4(0.7, 0, 1, 1));
+
+    for(var i = 0; i < rightFill.length-2; i++){
+        colors = colors.concat(vec4(1,1,1,1));
+    }
+    for(var i = 0; i < lensStripes.length; i++){
+        console.log(i%2);
+        if(i%2 == 0)
+        {
+            colors = colors.concat(vec4(0,0,0,1));
+        }
+        else
+        {
+            colors = colors.concat(vec4(1,1,1,1));
+        }
+    }
 
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
     var colorBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
->>>>>>> parent of 98e2598 (Added background and basic glasses)
-
-    smoothArrays = gl.createVertexArray();
-    gl.bindVertexArray(smoothArrays);
-
-
-
-<<<<<<< HEAD
-    // Load data into GPU data buffers
-
-    /* Position Buffer */
-    var smoothPositions = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, smoothPositions);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(smoothPositions), gl.STATIC_DRAW);
-
-    // Associate shader atributes with corresponding data buffers
-        var smoothvPosition = gl.getAttribLocation(smoothProgram, "vPosition");
-        gl.enableVertexAttribArray(smoothvPosition);
-        gl.vertexAttribPointer(smoothvPosition, 2, gl.FLOAT, gl.FALSE, 0, 0);
-
-
-
-    /* Color Buffer */
-    var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    //gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-    // Enable the shader's vertex color input and attach the active buffer
-    var vColor = gl.getAttribLocation(smoothProgram, "vColor");
+    myFirstVao = gl.createVertexArray();
+    gl.bindVertexArray(myFirstVao);
+
+    uColor = gl.getUniformLocation(program, "uColor");
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.enableVertexAttribArray(vPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+    //Enable the shader's vertex colour input and attach the active buffer
+    var vColor = gl.getAttribLocation(program, "vColor");
     gl.enableVertexAttribArray(vColor);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, gl.FALSE, 0, 0);
 
-    // Allocate a VAO to manage buffer connections to the shader
-    flatArrays = gl.createVertexArray();
-    gl.bindVertexArray(flatArrays);
-
-    // Points to draw with UNIFORM COLORS
-    var flatPoints = [];
-
-    /*
-        INSERT CONCATS FOR STRIPES AND BASIC GLASSES FRAME HERE
-    */
-    flatPoints = flatPoints.concat(bgStripes);
-
-    
-    leftLens.start = flatPoints.length;
-    flatPoints = flatPoints.concat(leftLens);
-    rightLens.start = flatPoints.length;
-    flatPoints = flatPoints.concat(rightLens);
-
-
-    // Create a buffer for vertex positions, make it active, and copy the data to it
-    var flatPositions = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, flatPositions);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(flatPoints), gl.STATIC_DRAW);
-
-    // Associate shader attributes with corresponding data buffers
-    var flatvPosition = gl.getAttribLocation(flatProgram, "vPosition");
-    gl.enableVertexAttribArray(flatvPosition);
-    gl.vertexAttribPointer(flatvPosition, 2, gl.FLOAT, gl.FALSE, 0, 0);
-
-    // Get addresses of shader uniforms
-    uColor = gl.getUniformLocation(flatProgram, "uColor");
-    ////// End of flat shader
-
-=======
-     //Enable the shader's vertex colour input and attach the active buffer
-     var vColor = gl.getAttribLocation( program, "vColor" );
-     gl.enableVertexAttribArray( vColor );
-     gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer );
-     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, gl.FALSE, 0, 0 );
-
-    gl.clearColor(0, 0, 0, 1);
->>>>>>> parent of 98e2598 (Added background and basic glasses)
+    gl.clearColor(0.4941176471, 0.6117647059, 0.6, 1.0);
 
     requestAnimationFrame(render);
 
@@ -222,56 +292,26 @@ function init() {
 
 
 function render() {
+    console.log();
     gl.clear(gl.COLOR_BUFFER_BIT);
-<<<<<<< HEAD
     // Background Stripes
-
-    // draw flat shading with color uniform
-    gl.useProgram(flatProgram);
-    gl.bindVertexArray(flatArrays);
-
-    // colors
-    var bgWhite = vec4(0.7647058824, 0.7137254902, 0.6862745098, 1);
-    var glassesFrame = vec4( 1, 0.7, 0.75, 1);
-
-    gl.uniform4fv(uColor, bgWhite);
-    for(var startNum = bgStripes.start; startNum > bgStripes.length; startNum += 4)
-    {
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 4, 4);    
-        gl.drawArrays(gl.TRIANGLE_STRIP, 8, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 12, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 16, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 20, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 24, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 28, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 32, 4);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 36, 4);
+    for (var i = 0; i < bgStripeCoords.length; i += 4) {
+        gl.drawArrays(gl.TRIANGLE_STRIP, i, 4);
     }
-    gl.uniform4fv(uColor, glassesFrame);
+
+    // Glasses
+    gl.drawArrays(gl.TRIANGLE_FAN, leftFill.start, leftFill.length);
+    gl.drawArrays(gl.TRIANGLE_FAN, rightFill.start, rightFill.length);
     gl.drawArrays(gl.TRIANGLE_STRIP, leftLens.start, leftLens.length);
     gl.drawArrays(gl.TRIANGLE_STRIP, rightLens.start, rightLens.length);
-
-=======
->>>>>>> parent of 98e2598 (Added background and basic glasses)
-
-    gl.uniform4f(uColor, 1, 1, 0, 1);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-    var green = vec4(0, .5, 0, 1);
-    gl.uniform4fv(uColor, flatten(green));
-
-    gl.lineWidth(10);
-    gl.drawArrays(gl.LINE_LOOP, 6, 3);
-
-    gl.lineWidth(5);
-    gl.drawArrays(gl.LINE_LOOP, 9, 3);
-
-    //gl.enable(gl.CULL_FACE);
+    gl.drawArrays(gl.TRIANGLE_STRIP, noseBar.start, noseBar.length);
+    gl.drawArrays(gl.POINTS, wings.start, wings.length);
+    gl.drawArrays(gl.LINES, lensStripes.start, lensStripes.length);
+    
 
 };
 
-function circle(sides) {
+function circle(sides, xMult1, xAdd1, yMult1, yAdd1, xMult2, xAdd2, yMult2, yAdd2) {
     var vertices = []; // create empty array
     if (sides < 3) {
         console.log("function circle: Not enough sides to make a polygon.");
@@ -282,7 +322,9 @@ function circle(sides) {
             console.log("function circle: Sides limited to 10,000.");
         }
         for (var i = sides; i >= 0; i--) {
-            vertices.push(vec2(Math.cos(i / sides * 2 * Math.PI), Math.sin(i / sides * 2 * Math.PI)));
+            vertices.push(vec2(Math.cos((i / sides * 2 * Math.PI)) * xMult1 + xAdd1, Math.sin((i / sides * 2 * Math.PI)) * yMult1 + yAdd1));
+            vertices.push(vec2(Math.cos(i / sides * 2 * Math.PI) * xMult2 + xAdd2, Math.sin(i / sides * 2 * Math.PI) * yMult2 + yAdd2));
+
         }
     }
     return vertices;
